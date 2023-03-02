cf93cdceaa1516f8c783a2adecbb3f2ea61791c9
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2023-03-02 15:18:22 -0600

    b

--- data/data.js
+++ data/data.js
@@ -1,8 +1,9 @@
+import noteArray from './notes.js';
 const sourceUrl = './note-frequencies.json';
 
-const noteArray = (await (await fetch(sourceUrl)).json()).notes;
+// const noteArray = (await (await fetch(sourceUrl)).json()).notes;
 
-const frequencyMap = new Map(noteArray.map(_ => [Math.trunc(_.frequency), _]))
+const frequencyMap = new Map(noteArray.notes.map(_ => [Math.trunc(_.frequency), _]))
 
 export const noteDataSets = {
   noteArray,
---
+++ data/notes.js
@@ -0,0 +1,868 @@
+export default JSON.parse(`{
+  "notes": [
+    {
+      "frequency": 16.35,
+      "wavelength": 2109.89,
+      "pitchClass": "C",
+      "pitch": "C0",
+      "octave": 0,
+      "index": 0
+    },
+    {
+      "frequency": 17.32,
+      "wavelength": 1991.47,
+      "pitchClass": "C#",
+      "pitch": "C#0",
+      "octave": 0,
+      "index": 1
+    },
+    {
+      "frequency": 18.35,
+      "wavelength": 1879.69,
+      "pitchClass": "D",
+      "pitch": "D0",
+      "octave": 0,
+      "index": 2
+    },
+    {
+      "frequency": 19.45,
+      "wavelength": 1774.2,
+      "pitchClass": "D#",
+      "pitch": "D#0",
+      "octave": 0,
+      "index": 3
+    },
+    {
+      "frequency": 20.6,
+      "wavelength": 1674.62,
+      "pitchClass": "E",
+      "pitch": "E0",
+      "octave": 0,
+      "index": 4
+    },
+    {
+      "frequency": 21.83,
+      "wavelength": 1580.63,
+      "pitchClass": "F",
+      "pitch": "F0",
+      "octave": 0,
+      "index": 5
+    },
+    {
+      "frequency": 23.12,
+      "wavelength": 1491.91,
+      "pitchClass": "F#",
+      "pitch": "F#0",
+      "octave": 0,
+      "index": 6
+    },
+    {
+      "frequency": 24.5,
+      "wavelength": 1408.18,
+      "pitchClass": "G",
+      "pitch": "G0",
+      "octave": 0,
+      "index": 7
+    },
+    {
+      "frequency": 25.96,
+      "wavelength": 1329.14,
+      "pitchClass": "G#",
+      "pitch": "G#0",
+      "octave": 0,
+      "index": 8
+    },
+    {
+      "frequency": 27.5,
+      "wavelength": 1254.55,
+      "pitchClass": "A",
+      "pitch": "A0",
+      "octave": 0,
+      "index": 9
+    },
+    {
+      "frequency": 29.14,
+      "wavelength": 1184.13,
+      "pitchClass": "A#",
+      "pitch": "A#0",
+      "octave": 0,
+      "index": 10
+    },
+    {
+      "frequency": 30.87,
+      "wavelength": 1117.67,
+      "pitchClass": "B",
+      "pitch": "B0",
+      "octave": 0,
+      "index": 11
+    },
+    {
+      "frequency": 32.7,
+      "wavelength": 1054.94,
+      "pitchClass": "C",
+      "pitch": "C1",
+      "octave": 1,
+      "index": 12
+    },
+    {
+      "frequency": 34.65,
+      "wavelength": 995.73,
+      "pitchClass": "C#",
+      "pitch": "C#1",
+      "octave": 1,
+      "index": 13
+    },
+    {
+      "frequency": 36.71,
+      "wavelength": 939.85,
+      "pitchClass": "D",
+      "pitch": "D1",
+      "octave": 1,
+      "index": 14
+    },
+    {
+      "frequency": 38.89,
+      "wavelength": 887.1,
+      "pitchClass": "D#",
+      "pitch": "D#1",
+      "octave": 1,
+      "index": 15
+    },
+    {
+      "frequency": 41.2,
+      "wavelength": 837.31,
+      "pitchClass": "E",
+      "pitch": "E1",
+      "octave": 1,
+      "index": 16
+    },
+    {
+      "frequency": 43.65,
+      "wavelength": 790.31,
+      "pitchClass": "F",
+      "pitch": "F1",
+      "octave": 1,
+      "index": 17
+    },
+    {
+      "frequency": 46.25,
+      "wavelength": 745.96,
+      "pitchClass": "F#",
+      "pitch": "F#1",
+      "octave": 1,
+      "index": 18
+    },
+    {
+      "frequency": 49,
+      "wavelength": 704.09,
+      "pitchClass": "G",
+      "pitch": "G1",
+      "octave": 1,
+      "index": 19
+    },
+    {
+      "frequency": 51.91,
+      "wavelength": 664.57,
+      "pitchClass": "G#",
+      "pitch": "G#1",
+      "octave": 1,
+      "index": 20
+    },
+    {
+      "frequency": 55,
+      "wavelength": 627.27,
+      "pitchClass": "A",
+      "pitch": "A1",
+      "octave": 1,
+      "index": 21
+    },
+    {
+      "frequency": 58.27,
+      "wavelength": 592.07,
+      "pitchClass": "A#",
+      "pitch": "A#1",
+      "octave": 1,
+      "index": 22
+    },
+    {
+      "frequency": 61.74,
+      "wavelength": 558.84,
+      "pitchClass": "B",
+      "pitch": "B1",
+      "octave": 1,
+      "index": 23
+    },
+    {
+      "frequency": 65.41,
+      "wavelength": 527.47,
+      "pitchClass": "C",
+      "pitch": "C2",
+      "octave": 2,
+      "index": 24
+    },
+    {
+      "frequency": 69.3,
+      "wavelength": 497.87,
+      "pitchClass": "C#",
+      "pitch": "C#2",
+      "octave": 2,
+      "index": 25
+    },
+    {
+      "frequency": 73.42,
+      "wavelength": 469.92,
+      "pitchClass": "D",
+      "pitch": "D2",
+      "octave": 2,
+      "index": 26
+    },
+    {
+      "frequency": 77.78,
+      "wavelength": 443.55,
+      "pitchClass": "D#",
+      "pitch": "D#2",
+      "octave": 2,
+      "index": 27
+    },
+    {
+      "frequency": 82.41,
+      "wavelength": 418.65,
+      "pitchClass": "E",
+      "pitch": "E2",
+      "octave": 2,
+      "index": 28
+    },
+    {
+      "frequency": 87.31,
+      "wavelength": 395.16,
+      "pitchClass": "F",
+      "pitch": "F2",
+      "octave": 2,
+      "index": 29
+    },
+    {
+      "frequency": 92.5,
+      "wavelength": 372.98,
+      "pitchClass": "F#",
+      "pitch": "F#2",
+      "octave": 2,
+      "index": 30
+    },
+    {
+      "frequency": 98,
+      "wavelength": 352.04,
+      "pitchClass": "G",
+      "pitch": "G2",
+      "octave": 2,
+      "index": 31
+    },
+    {
+      "frequency": 103.83,
+      "wavelength": 332.29,
+      "pitchClass": "G#",
+      "pitch": "G#2",
+      "octave": 2,
+      "index": 32
+    },
+    {
+      "frequency": 110,
+      "wavelength": 313.64,
+      "pitchClass": "A",
+      "pitch": "A2",
+      "octave": 2,
+      "index": 33
+    },
+    {
+      "frequency": 116.54,
+      "wavelength": 296.03,
+      "pitchClass": "A#",
+      "pitch": "A#2",
+      "octave": 2,
+      "index": 34
+    },
+    {
+      "frequency": 123.47,
+      "wavelength": 279.42,
+      "pitchClass": "B",
+      "pitch": "B2",
+      "octave": 2,
+      "index": 35
+    },
+    {
+      "frequency": 130.81,
+      "wavelength": 263.74,
+      "pitchClass": "C",
+      "pitch": "C3",
+      "octave": 3,
+      "index": 36
+    },
+    {
+      "frequency": 138.59,
+      "wavelength": 248.93,
+      "pitchClass": "C#",
+      "pitch": "C#3",
+      "octave": 3,
+      "index": 37
+    },
+    {
+      "frequency": 146.83,
+      "wavelength": 234.96,
+      "pitchClass": "D",
+      "pitch": "D3",
+      "octave": 3,
+      "index": 38
+    },
+    {
+      "frequency": 155.56,
+      "wavelength": 221.77,
+      "pitchClass": "D#",
+      "pitch": "D#3",
+      "octave": 3,
+      "index": 39
+    },
+    {
+      "frequency": 164.81,
+      "wavelength": 209.33,
+      "pitchClass": "E",
+      "pitch": "E3",
+      "octave": 3,
+      "index": 40
+    },
+    {
+      "frequency": 174.61,
+      "wavelength": 197.58,
+      "pitchClass": "F",
+      "pitch": "F3",
+      "octave": 3,
+      "index": 41
+    },
+    {
+      "frequency": 185,
+      "wavelength": 186.49,
+      "pitchClass": "F#",
+      "pitch": "F#3",
+      "octave": 3,
+      "index": 42
+    },
+    {
+      "frequency": 196,
+      "wavelength": 176.02,
+      "pitchClass": "G",
+      "pitch": "G3",
+      "octave": 3,
+      "index": 43
+    },
+    {
+      "frequency": 207.65,
+      "wavelength": 166.14,
+      "pitchClass": "G#",
+      "pitch": "G#3",
+      "octave": 3,
+      "index": 44
+    },
+    {
+      "frequency": 220,
+      "wavelength": 156.82,
+      "pitchClass": "A",
+      "pitch": "A3",
+      "octave": 3,
+      "index": 45
+    },
+    {
+      "frequency": 233.08,
+      "wavelength": 148.02,
+      "pitchClass": "A#",
+      "pitch": "A#3",
+      "octave": 3,
+      "index": 46
+    },
+    {
+      "frequency": 246.94,
+      "wavelength": 139.71,
+      "pitchClass": "B",
+      "pitch": "B3",
+      "octave": 3,
+      "index": 47
+    },
+    {
+      "frequency": 261.63,
+      "wavelength": 131.87,
+      "pitchClass": "C",
+      "pitch": "C4",
+      "octave": 4,
+      "index": 48
+    },
+    {
+      "frequency": 277.18,
+      "wavelength": 124.47,
+      "pitchClass": "C#",
+      "pitch": "C#4",
+      "octave": 4,
+      "index": 49
+    },
+    {
+      "frequency": 293.66,
+      "wavelength": 117.48,
+      "pitchClass": "D",
+      "pitch": "D4",
+      "octave": 4,
+      "index": 50
+    },
+    {
+      "frequency": 311.13,
+      "wavelength": 110.89,
+      "pitchClass": "D#",
+      "pitch": "D#4",
+      "octave": 4,
+      "index": 51
+    },
+    {
+      "frequency": 329.63,
+      "wavelength": 104.66,
+      "pitchClass": "E",
+      "pitch": "E4",
+      "octave": 4,
+      "index": 52
+    },
+    {
+      "frequency": 349.23,
+      "wavelength": 98.79,
+      "pitchClass": "F",
+      "pitch": "F4",
+      "octave": 4,
+      "index": 53
+    },
+    {
+      "frequency": 369.99,
+      "wavelength": 93.24,
+      "pitchClass": "F#",
+      "pitch": "F#4",
+      "octave": 4,
+      "index": 54
+    },
+    {
+      "frequency": 392,
+      "wavelength": 88.01,
+      "pitchClass": "G",
+      "pitch": "G4",
+      "octave": 4,
+      "index": 55
+    },
+    {
+      "frequency": 415.3,
+      "wavelength": 83.07,
+      "pitchClass": "G#",
+      "pitch": "G#4",
+      "octave": 4,
+      "index": 56
+    },
+    {
+      "frequency": 440,
+      "wavelength": 78.41,
+      "pitchClass": "A",
+      "pitch": "A4",
+      "octave": 4,
+      "index": 57
+    },
+    {
+      "frequency": 466.16,
+      "wavelength": 74.01,
+      "pitchClass": "A#",
+      "pitch": "A#4",
+      "octave": 4,
+      "index": 58
+    },
+    {
+      "frequency": 493.88,
+      "wavelength": 69.85,
+      "pitchClass": "B",
+      "pitch": "B4",
+      "octave": 4,
+      "index": 59
+    },
+    {
+      "frequency": 523.25,
+      "wavelength": 65.93,
+      "pitchClass": "C",
+      "pitch": "C5",
+      "octave": 5,
+      "index": 60
+    },
+    {
+      "frequency": 554.37,
+      "wavelength": 62.23,
+      "pitchClass": "C#",
+      "pitch": "C#5",
+      "octave": 5,
+      "index": 61
+    },
+    {
+      "frequency": 587.33,
+      "wavelength": 58.74,
+      "pitchClass": "D",
+      "pitch": "D5",
+      "octave": 5,
+      "index": 62
+    },
+    {
+      "frequency": 622.25,
+      "wavelength": 55.44,
+      "pitchClass": "D#",
+      "pitch": "D#5",
+      "octave": 5,
+      "index": 63
+    },
+    {
+      "frequency": 659.25,
+      "wavelength": 52.33,
+      "pitchClass": "E",
+      "pitch": "E5",
+      "octave": 5,
+      "index": 64
+    },
+    {
+      "frequency": 698.46,
+      "wavelength": 49.39,
+      "pitchClass": "F",
+      "pitch": "F5",
+      "octave": 5,
+      "index": 65
+    },
+    {
+      "frequency": 739.99,
+      "wavelength": 46.62,
+      "pitchClass": "F#",
+      "pitch": "F#5",
+      "octave": 5,
+      "index": 66
+    },
+    {
+      "frequency": 783.99,
+      "wavelength": 44.01,
+      "pitchClass": "G",
+      "pitch": "G5",
+      "octave": 5,
+      "index": 67
+    },
+    {
+      "frequency": 830.61,
+      "wavelength": 41.54,
+      "pitchClass": "G#",
+      "pitch": "G#5",
+      "octave": 5,
+      "index": 68
+    },
+    {
+      "frequency": 880,
+      "wavelength": 39.2,
+      "pitchClass": "A",
+      "pitch": "A5",
+      "octave": 5,
+      "index": 69
+    },
+    {
+      "frequency": 932.33,
+      "wavelength": 37,
+      "pitchClass": "A#",
+      "pitch": "A#5",
+      "octave": 5,
+      "index": 70
+    },
+    {
+      "frequency": 987.77,
+      "wavelength": 34.93,
+      "pitchClass": "B",
+      "pitch": "B5",
+      "octave": 5,
+      "index": 71
+    },
+    {
+      "frequency": 1046.5,
+      "wavelength": 32.97,
+      "pitchClass": "C",
+      "pitch": "C6",
+      "octave": 6,
+      "index": 72
+    },
+    {
+      "frequency": 1108.73,
+      "wavelength": 31.12,
+      "pitchClass": "C#",
+      "pitch": "C#6",
+      "octave": 6,
+      "index": 73
+    },
+    {
+      "frequency": 1174.66,
+      "wavelength": 29.37,
+      "pitchClass": "D",
+      "pitch": "D6",
+      "octave": 6,
+      "index": 74
+    },
+    {
+      "frequency": 1244.51,
+      "wavelength": 27.72,
+      "pitchClass": "D#",
+      "pitch": "D#6",
+      "octave": 6,
+      "index": 75
+    },
+    {
+      "frequency": 1318.51,
+      "wavelength": 26.17,
+      "pitchClass": "E",
+      "pitch": "E6",
+      "octave": 6,
+      "index": 76
+    },
+    {
+      "frequency": 1396.91,
+      "wavelength": 24.7,
+      "pitchClass": "F",
+      "pitch": "F6",
+      "octave": 6,
+      "index": 77
+    },
+    {
+      "frequency": 1479.98,
+      "wavelength": 23.31,
+      "pitchClass": "F#",
+      "pitch": "F#6",
+      "octave": 6,
+      "index": 78
+    },
+    {
+      "frequency": 1567.98,
+      "wavelength": 22,
+      "pitchClass": "G",
+      "pitch": "G6",
+      "octave": 6,
+      "index": 79
+    },
+    {
+      "frequency": 1661.22,
+      "wavelength": 20.77,
+      "pitchClass": "G#",
+      "pitch": "G#6",
+      "octave": 6,
+      "index": 80
+    },
+    {
+      "frequency": 1760,
+      "wavelength": 19.6,
+      "pitchClass": "A",
+      "pitch": "A6",
+      "octave": 6,
+      "index": 81
+    },
+    {
+      "frequency": 1864.66,
+      "wavelength": 18.5,
+      "pitchClass": "A#",
+      "pitch": "A#6",
+      "octave": 6,
+      "index": 82
+    },
+    {
+      "frequency": 1975.53,
+      "wavelength": 17.46,
+      "pitchClass": "B",
+      "pitch": "B6",
+      "octave": 6,
+      "index": 83
+    },
+    {
+      "frequency": 2093,
+      "wavelength": 16.48,
+      "pitchClass": "C",
+      "pitch": "C7",
+      "octave": 7,
+      "index": 84
+    },
+    {
+      "frequency": 2217.46,
+      "wavelength": 15.56,
+      "pitchClass": "C#",
+      "pitch": "C#7",
+      "octave": 7,
+      "index": 85
+    },
+    {
+      "frequency": 2349.32,
+      "wavelength": 14.69,
+      "pitchClass": "D",
+      "pitch": "D7",
+      "octave": 7,
+      "index": 86
+    },
+    {
+      "frequency": 2489.02,
+      "wavelength": 13.86,
+      "pitchClass": "D#",
+      "pitch": "D#7",
+      "octave": 7,
+      "index": 87
+    },
+    {
+      "frequency": 2637.02,
+      "wavelength": 13.08,
+      "pitchClass": "E",
+      "pitch": "E7",
+      "octave": 7,
+      "index": 88
+    },
+    {
+      "frequency": 2793.83,
+      "wavelength": 12.35,
+      "pitchClass": "F",
+      "pitch": "F7",
+      "octave": 7,
+      "index": 89
+    },
+    {
+      "frequency": 2959.96,
+      "wavelength": 11.66,
+      "pitchClass": "F#",
+      "pitch": "F#7",
+      "octave": 7,
+      "index": 90
+    },
+    {
+      "frequency": 3135.96,
+      "wavelength": 11,
+      "pitchClass": "G",
+      "pitch": "G7",
+      "octave": 7,
+      "index": 91
+    },
+    {
+      "frequency": 3322.44,
+      "wavelength": 10.38,
+      "pitchClass": "G#",
+      "pitch": "G#7",
+      "octave": 7,
+      "index": 92
+    },
+    {
+      "frequency": 3520,
+      "wavelength": 9.8,
+      "pitchClass": "A",
+      "pitch": "A7",
+      "octave": 7,
+      "index": 93
+    },
+    {
+      "frequency": 3729.31,
+      "wavelength": 9.25,
+      "pitchClass": "A#",
+      "pitch": "A#7",
+      "octave": 7,
+      "index": 94
+    },
+    {
+      "frequency": 3951.07,
+      "wavelength": 8.73,
+      "pitchClass": "B",
+      "pitch": "B7",
+      "octave": 7,
+      "index": 95
+    },
+    {
+      "frequency": 4186.01,
+      "wavelength": 8.24,
+      "pitchClass": "C",
+      "pitch": "C8",
+      "octave": 8,
+      "index": 96
+    },
+    {
+      "frequency": 4434.92,
+      "wavelength": 7.78,
+      "pitchClass": "C#",
+      "pitch": "C#8",
+      "octave": 8,
+      "index": 97
+    },
+    {
+      "frequency": 4698.63,
+      "wavelength": 7.34,
+      "pitchClass": "D",
+      "pitch": "D8",
+      "octave": 8,
+      "index": 98
+    },
+    {
+      "frequency": 4978.03,
+      "wavelength": 6.93,
+      "pitchClass": "D#",
+      "pitch": "D#8",
+      "octave": 8,
+      "index": 99
+    },
+    {
+      "frequency": 5274.04,
+      "wavelength": 6.54,
+      "pitchClass": "E",
+      "pitch": "E8",
+      "octave": 8,
+      "index": 100
+    },
+    {
+      "frequency": 5587.65,
+      "wavelength": 6.17,
+      "pitchClass": "F",
+      "pitch": "F8",
+      "octave": 8,
+      "index": 101
+    },
+    {
+      "frequency": 5919.91,
+      "wavelength": 5.83,
+      "pitchClass": "F#",
+      "pitch": "F#8",
+      "octave": 8,
+      "index": 102
+    },
+    {
+      "frequency": 6271.93,
+      "wavelength": 5.5,
+      "pitchClass": "G",
+      "pitch": "G8",
+      "octave": 8,
+      "index": 103
+    },
+    {
+      "frequency": 6644.88,
+      "wavelength": 5.19,
+      "pitchClass": "G#",
+      "pitch": "G#8",
+      "octave": 8,
+      "index": 104
+    },
+    {
+      "frequency": 7040,
+      "wavelength": 4.9,
+      "pitchClass": "A",
+      "pitch": "A8",
+      "octave": 8,
+      "index": 105
+    },
+    {
+      "frequency": 7458.62,
+      "wavelength": 4.63,
+      "pitchClass": "A#",
+      "pitch": "A#8",
+      "octave": 8,
+      "index": 106
+    },
+    {
+      "frequency": 7902.13,
+      "wavelength": 4.37,
+      "pitchClass": "B",
+      "pitch": "B8",
+      "octave": 8,
+      "index": 107
+  }
+]
+}`)
